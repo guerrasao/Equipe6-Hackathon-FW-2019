797fd96788e5cb5788bef03abd41e1ed6331cd20
LeonardoZancanaro <nadobz@gmail.com>
2019-11-30 16:33:10 -0300

    Update salva_pedido.motorista.php

--- perfis/motorista/salva_pedido.motorista.php
+++ perfis/motorista/salva_pedido.motorista.php
@@ -1,5 +1,9 @@
 <?php
 $path = $_SERVER['DOCUMENT_ROOT'] . '/Equipe6-Hackathon-FW-2019/';
+include_once $path."BD/conexao.php";
+include_once $path."BD/funcoes_iniciais.php";
+$con1 = AbrirConexao();
+$pedido = $_POST['id_pedido'];
 if($_FILES[ 'arquivo' ][ 'name' ][0] != '') {
     foreach ($_FILES['arquivo']['name'] as $key => $file) {
         if($_FILES[ 'arquivo' ][ 'error' ][$key] == 0){
@@ -12,15 +16,18 @@ if($_FILES[ 'arquivo' ][ 'name' ][0] != '') {
             //if ( strstr ( '.jpg;.jpeg;.gif;.png', $extensao ) ) {
             $novoNome = uniqid ( time () ) . '.' . $extensao;
             $destino = $path.'imagens_entrega/' . $novoNome;
+
+            
+            $query = mysqli_query($con1, "INSERT INTO imagens (id_imagem, nome_arquivo, id_pedido, tipo_imagem) VALUES (null, '$novoNome', $pedido, null)");
             move_uploaded_file( $arquivo_tmp, $destino);
             //}
             
         }
         
     }
-    header('location:pedido.motorista.php?mensagem=imagens_salvas');
+    header('location:pedido.motorista.php?mensagem=imagens_salvas&id_pedido='.$pedido);
 }else{
-    header('location:pedido.motorista.php?erro=n_selecionou_arquivos');
+    header('location:pedido.motorista.php?erro=n_selecionou_arquivos&id_pedido='.$pedido);
 }
 //header('location:pedido.motorista.php');
 /*$destino = $path.'imagens_entrega/' . $_FILES['arquivo']['name'];
