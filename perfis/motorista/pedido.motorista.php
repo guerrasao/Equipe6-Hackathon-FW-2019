fcc0ce6cd053e94a93bda891dd61ce00b0988035
guerrasao <patryckguerra@yahoo.com.br>
2019-11-30 16:22:24 -0300

    Update pedido.motorista.php

--- perfis/motorista/pedido.motorista.php
+++ perfis/motorista/pedido.motorista.php
@@ -75,24 +75,3 @@ $path = $_SERVER['DOCUMENT_ROOT'] . '/Equipe6-Hackathon-FW-2019';
 $rodape = $path . '/cabecalho/rodape.usuario.php';
 include_once($rodape);
 ?>
-
-<section class="container p-3 col-md-3 mt-5 mb-5 md-100">
-    <div class="cabecalho-box">
-        <h3 class="mb-0">Pedido</h3>
-    </div>
-    <form method="post" enctype="multipart/form-data" action="salva_pedido.motorista.php">
-        Selecione uma imagem: <input name="arquivo[]" id="filesToUpload" type="file" multiple="" />
-        <br />
-        <input type="submit" value="Salvar" />
-    </form>
-    <form method="post" enctype="multipart/form-data" action="assina.motorista.php">
-        <input name="assinatura" type="hidden" value="1" />
-        <br>
-        Entrega Concluída? <input type="submit" value="Assinar" />
-    </form>
-</section>
-
-<?php
-$rodape = $path . 'rodape.php';
-include_once($rodape);
-?>
