089620994e55ca58ada7c5e8fd6f4b057ea69ceb
guerrasao <patryckguerra@yahoo.com.br>
2019-11-30 05:45:15 -0300

    atulização cabeçalhos

--- perfis/motorista/index.motorista.php
+++ perfis/motorista/index.motorista.php
@@ -11,14 +11,14 @@ include_once($cabecalho);
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Blank Page</h1>
+            <h1>Titulo</h1>
           </div>
-          <div class="col-sm-6">
+          <!-- <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
               <li class="breadcrumb-item active">Blank Page</li>
             </ol>
-          </div>
+          </div> -->
         </div>
       </div><!-- /.container-fluid -->
     </section>
@@ -34,8 +34,8 @@ include_once($cabecalho);
           <div class="card-tools">
             <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
               <i class="fas fa-minus"></i></button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fas fa-times"></i></button>
+            <!-- <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+              <i class="fas fa-times"></i></button> -->
           </div>
         </div>
         <div class="card-body">
